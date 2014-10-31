cc787906b6680b5f7ce0faf272101975a01b3397
Tristan Harris <tristan@mohc.net>
2014-10-31 14:01:31 +0000

    More string escaping & improves output handling functions

--- global_functions.php
+++ global_functions.php
@@ -9,7 +9,31 @@
  * h
  *
  * For outputting strings. Add all sorts of clean methods in here as desired.
+ * either takes one arg of text to escape
+ * or printf style args where ALL args apart from the formatter are escaped
+ * eg. h('<h1>%s</h1><p>%s</p>', $v1, $v2)
  */
-function h($text) {
-	return htmlspecialchars($text);
+function h($text /*args*/) {
+	$args = func_get_args();
+	if (count($args) == 1) {
+		return htmlspecialchars($text);
+	} else {
+		$args = array_map(function($str) { return htmlspecialchars($str); }, $args);
+		$args[0] = $text;
+		return call_user_func_array('sprintf', $args);
+	}
+}
+
+/**
+ * hx
+ *
+ * Outputs stings using h() above unless all the arguments are empty
+ * Note pass in args using @ if the variable may be unset (eg hx('<p>%s</p>', @$obj->missing))
+ */
+function hx($text /*args*/) {
+	$args = func_get_args();
+	array_shift($args);
+	if (join($args) === '') return '';
+	array_unshift($args, $text);
+	return call_user_func_array('h', $args);
 }
